bed0836a649029694ec3dd46757f73bf4e7c2509
NaoLizanoG <naomilizano@gmail.com>
2023-04-12 21:38:01 -0500

    final con comentarios

--- grupo5suma.c
+++ grupo5suma.c
@@ -1,20 +1,21 @@
 //Cadena, Flores y Lizano
 #include <stdio.h>
 
-
-
 int main() {
+    //variables
     int n, i, a=1;
     float sum = 0;
 
+    //se le pide al usuario que ingrese un numero y se lo guarda en una variable
     printf("Ingrese un número entero positivo: \n");
     scanf("%d", &n);
 
+    //se valida que el numero sea positivo
     while (n<0){
      printf("Cantidad invalida, ingrese otro número \n");
     scanf("%d", &n);    
     }
-
+    //operacion para calcular la suma
     for(i = 1; i <= n; i++) {
 
         if(i%2!=0){
