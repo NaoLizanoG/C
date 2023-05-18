9de4def966ce8e1a1b3b85a42cbb7427cc14ace7
Nao LizanoG <naomilizano@gmail.com>
2023-05-18 12:11:40 -0500

    Version final con comentarios

--- semana 8/matriz-identidad.c
+++ semana 8/matriz-identidad.c
@@ -3,12 +3,15 @@
 int main (){
 int filas, columnas, cont;
 cont=0;
+
+//Se pide al usuario que ingrese los datos de la matriz
 printf("Ingrese el numero de filas de la matriz\n");
 scanf("%d", &filas);
 printf("Ingrese el numero de columnas de la matriz\n");
 scanf("%d", &columnas);
 int A[filas][columnas];
 
+//Se determina que la matriz solo tiene ceros
 for (int i = 0; i < filas; i++)
 {
     for (int j = 0; j < columnas; j++)
@@ -29,14 +32,18 @@ for (int i = 0; i < filas; i++)
     }
     
 
+//Se determina que la diagonal de la matriz solo tenga unos
     printf("\n La matriz con una diagonal de unos es:\n");
 for (int i = 0; i < filas; i++)
 {
     for (int j = 0; j < columnas; j++)
     {
+       
         if (i==j)
         {
              A[i][j]=1;
+             
+              //if para que los resultados salgan en forma de matriz
              if (cont==columnas)
         {
          printf("\n %d ", A[i][j] );
@@ -48,6 +55,7 @@ for (int i = 0; i < filas; i++)
         
         cont=cont+1;
         }
+
         else{
         A[i][j]=0;
        if (cont==columnas)
