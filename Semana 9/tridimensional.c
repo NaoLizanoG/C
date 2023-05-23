11fafb804fe29f6923a5d41959df45094a77afbf
Nao LizanoG <naomilizano@gmail.com>
2023-05-23 12:19:19 -0500

    Version final

--- Semana 9/tridimensional.c
+++ Semana 9/tridimensional.c
@@ -1,15 +1,20 @@
 # include<stdio.h>
 
 int main (){
+    //Se definene variables
 int matrices, filas, columnas;
+
+//Se pide que el usuario ingrese el tamaño de la matriz
 printf("Ingrese el numero de matrices:");
 scanf("%d", &matrices);
 printf("Ingrese el numero de filas:");
 scanf("%d", &filas);
 printf("Ingrese el numero de columnas:");
 scanf("%d", &columnas);
+printf ("\n");
 int A[matrices][filas][columnas];
 
+//Se inicializa la matriz 
 for (int i = 0; i < matrices; i++)
 {
     printf("La matriz %d es:\n", i+1);
@@ -17,11 +22,14 @@ for (int i = 0; i < matrices; i++)
     {
         for (int k = 0; k < columnas; k++)
         {
+            //If para inicializar en cero las primeras amtrices
             if (i+1<matrices){
+
+                //If para formar la matriz
             if (k+1==columnas)
             {
              A[i][j][k]=0;
-            printf(" %d\n", A[i][j][k]);
+            printf("%d\n", A[i][j][k]);
             }
             else{
                 A[i][j][k]=0;
@@ -29,11 +37,13 @@ for (int i = 0; i < matrices; i++)
             }
           
         }
+        //else para inicializar en 1 la ultima matriz
         else {
+            //if para formar la matriz
            if (k+1==columnas)
             {
              A[i][j][k]=1;
-            printf(" %d\n", A[i][j][k]);
+            printf("%d\n", A[i][j][k]);
             }
             else{
                 A[i][j][k]=1;
