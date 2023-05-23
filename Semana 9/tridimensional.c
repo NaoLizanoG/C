4a7ffdf3d83cc1dd9207cd91d4998d4373a015da
Nao LizanoG <naomilizano@gmail.com>
2023-05-23 12:14:38 -0500

    Solucion final completa

--- Semana 9/tridimensional.c
+++ Semana 9/tridimensional.c
@@ -12,19 +12,39 @@ int A[matrices][filas][columnas];
 
 for (int i = 0; i < matrices; i++)
 {
+    printf("La matriz %d es:\n", i+1);
     for (int j = 0; j < filas; j++)
     {
         for (int k = 0; k < columnas; k++)
         {
-            A[i][j][k]=0;
+            if (i+1<matrices){
+            if (k+1==columnas)
+            {
+             A[i][j][k]=0;
+            printf(" %d\n", A[i][j][k]);
+            }
+            else{
+                A[i][j][k]=0;
             printf("%d", A[i][j][k]);
+            }
+          
+        }
+        else {
+           if (k+1==columnas)
+            {
+             A[i][j][k]=1;
+            printf(" %d\n", A[i][j][k]);
+            }
+            else{
+                A[i][j][k]=1;
+            printf("%d", A[i][j][k]);
+            } 
+        }
         }
         
     }
     printf("\n");
     
-}
-
-    
+} 
 
 }
