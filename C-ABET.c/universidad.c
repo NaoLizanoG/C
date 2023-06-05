a7903f9b22d696c65445ff949b031317c67b872d
Nao LizanoG <naomilizano@gmail.com>
2023-06-05 16:56:09 -0500

    Solucion dos, alumnos que entraron en cada carrera cada periodo anual

--- C-ABET.c/universidad.c
+++ C-ABET.c/universidad.c
@@ -27,47 +27,46 @@ c) ¿En qué año la carrera de Ingeniería de Software recibió la mayor cantid
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(){
-    int alumnos[6][4];
-printf("El numero de alumnos de cada carrera es\n");
-for (int i = 0; i < 7; i++)
-{
-        switch (i+1)
-        {
-        case 1:
-            printf("Ingenieria en Software:\n");
-            break;
-        case 2:
-            printf("Administracion:\n");
-            break;
-        case 3:
-            printf("Economia:\n");
-            break;
-        case 4:
-            printf("Relaciones internacionales:\n");
-            break;
-        case 5:
-            printf("Matematicas:\n");
-            break;
-        case 6:
-            printf("Contabilidad:\n");
-            break;
-        case 7:
-            printf("Ingenieria industrial:\n");
-            break;
+int main() {
+    int alumnos[7][5];  
+
+    srand(time(NULL)); 
+    printf("El número de alumnos de cada carrera es:\n");
+
+    for (int i = 0; i < 7; i++) {
+        switch (i + 1) {
+            case 1:
+                printf("Ingenieria en Software:\n");
+                break;
+            case 2:
+                printf("Administracion:\n");
+                break;
+            case 3:
+                printf("Economia:\n");
+                break;
+            case 4:
+                printf("Relaciones internacionales:\n");
+                break;
+            case 5:
+                printf("Matemáticas:\n");
+                break;
+            case 6:
+                printf("Contabilidad:\n");
+                break;
+            case 7:
+                printf("Ingenieria industrial:\n");
+                break;
         }
-    for (int j = 0; j < 5; j++)
-    {
-        alumnos[i][j]=rand()%201;
-        printf("%d", j);
-        printf("periodo anual %d: %d\n",j+1, alumnos[i][j]);
 
+        for (int j = 0; j < 5; j++) {
+            alumnos[i][j] = rand() % 101;
+            printf("Periodo anual %d: %d\n", j + 1, alumnos[i][j]);
+        }
+        
+        printf("\n");  
     }
-    
-}
-
-
 
     return 0;
 }
